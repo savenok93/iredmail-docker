fa03560a49337dfecf82ef6a00b4337ae45f2e13
max_im_ka <savenok.max@gmail.com>
2020-02-28 12:38:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #docker rmi iredmail --force
 #docker build --no-cache  -t iredmail:latest build
-docker build -t iredmail:centos mysql/
-docker run --rm -ti --name iredmail -h mail.lejmr.com -e MYSQL_ROOT_PASSWORD=milos -e POSTMASTER_PASSWORD={PLAIN}heslo -p 80:80 -p 443:443 iredmail:centos
+docker build -t iredmail:ubuntu mysql/
+docker run --rm -ti --name iredmail -h mail.example.com -e MYSQL_ROOT_PASSWORD=yourpass -e POSTMASTER_PASSWORD={PLAIN}pass -p 80:80 -p 443:443 iredmail:ubuntu
 
 
 #rm -rf tmp/mysql/* tmp/vmail/*
@@ -13,27 +13,27 @@ docker run --rm -ti --name iredmail -h mail.lejmr.com -e MYSQL_ROOT_PASSWORD=mil
 #docker run --privileged -p 80:80 -p 443:443 --name=iredmail iredmail:latest /sbin/my_init
 
 # docker run --privileged -p 80:80 -p 443:443 \
-#            -h mail.lejmr.com \
+#            -h mail.example.com \
 #            -e "SOGO_WORKERS=1" \
 #            -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
 #            -e "TIMEZONE=Europe/Prague" \
-#            -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
-#            -e "MYSQL_ROOT_PASSWORD=heslo" \
-#            --name=iredmail iredmail:latest
+#            -e "POSTMASTER_PASSWORD={PLAIN}pass" \
+#            -e "MYSQL_ROOT_PASSWORD=pass" \
+#            --name=iredmail iredmail:ubuntu
            
            
 # exit 0
 
 # docker run --privileged -p 80:80 -p 443:443 \
-#            -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+#            -e "DOMAIN=example.com" -e "HOSTNAME=mail" \
 #            -e "SOGO_WORKERS=1" \
 #            -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
-#            -e "TIMEZONE=Europe/Prague" \
+#            -e "TIMEZONE=Europe/Bratislava" \
 #            -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
-#            -e "MYSQL_ROOT_PASSWORD=heslo2" \
+#            -e "MYSQL_ROOT_PASSWORD=pass2" \
 #            -v /home/milos/tmp/iredmail/lejmr/tmp/vmail:/var/vmail \
 #            -v /home/milos/tmp/iredmail/lejmr/tmp/clamav:/var/lib/clamav \
-#            --name=iredmail iredmail:latest
+#            --name=iredmail iredmail:ubuntu
 
 # #           -v /Users/milos/tmp/iredmail/lejmr/tmp/mysql:/var/lib/mysql \
 # #  {SSHA}Q61OCjQ8niwkvZEoeeEusokemJhvC5QLCHh9Qg==
